f8e4474be54c9e83ae63005a01e9a9e6961e7d4f
SHAMIM KHAN <shamimkhan@Shamim-MackBook-Air.local>
2024-05-27 00:40:17 +0530

    add the List Interview Question

---
+++ list.py
@@ -0,0 +1,44 @@
+list = ["apple", "banana","pen", "Book","Car"]
+
+list3 = [100, 45, 86, 32, 18, 29]
+#print(list[0])
+
+#list.pop()
+#list.remove("pen")
+
+# we use del keyword
+#del list[1]
+#del list
+
+# we use clear() Method
+#list.clear()
+
+# we use sort() method 
+
+#list2.sort()
+
+#we use sort Descending
+
+#list2.sort(reverse=True)
+
+#we use copy() Method 
+
+#my_list = list2.copy()
+
+# thislist = ["apple", "banana", "cherry"]
+# mylist = list(thislist)
+# print(mylist)
+
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
+
+
+
+
+
