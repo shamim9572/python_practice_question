f22a84a28ca53991e1ebec02a6ef87e896a20c35
SHAMIM KHAN <shamimkhan@Shamim-MackBook-Air.local>
2024-05-31 01:02:38 +0530

    Add the new file problem

--- dic.py
+++ dic.py
@@ -14,12 +14,29 @@
 # }
 # print(thisdict)
 
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964,
-  "year": 2020
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964,
+#   "year": 2020
+# }
+# print(thisdict)
+
+
+
+#Nasted Dic--------------------
+
+course = {
+  'php': {'duration':'3 months', 'fees': 15000},
+ 'Java': {'duration':'2 months', 'fees': 11000},
+ 'Python': {'duration':'4 months', 'fees': 15000},
+ 
 }
-print(thisdict)
+print(course['php'] ['fees'])
+
+for k,v in course.items():
+  print(k,v['duration'], v['fees']) 
+
+
 
 
---
+++ list_comp.py
@@ -0,0 +1,11 @@
+# l = []
+# for i in range(1, 101):
+#   l.append(i)
+  
+# print(l)
+
+
+#List Comprehension use
+
+n=[m for m in range(1,101) if m%2==0]
+print(n)
